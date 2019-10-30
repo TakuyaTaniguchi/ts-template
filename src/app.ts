6fcd68bed01932d4bbe1df5020129b204b042909
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-10-30 22:13:11 +0900

    Add c3

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,24 @@
+//template
 import {Item} from './item';
+import { isoParse } from 'd3';
 const elem = document.getElementById('output');
-const aBook = new Item('はじめてのTypeScript',1980);
+const aBook = new Item('はじめてのTypeScript',110);
 aBook.say(elem);
+
+//c3
+
+import * as c3 from 'c3'
+
+let chart = c3.generate({
+    bindto: '#chart',
+    data: {
+        type: 'donut',
+        columns: [
+            ['いい感じ',50],
+            ['イマイチ',50]
+        ]
+    },
+    donut: {
+        title: 'Typescriptの印象は？'
+    }
+})
